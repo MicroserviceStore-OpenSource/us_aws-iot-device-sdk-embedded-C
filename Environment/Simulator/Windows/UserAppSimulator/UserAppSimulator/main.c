96d17439cb84026f4ab69db3e705cbb3019a406f
DEV | Microservice Store <info@microservicestore.com>
2026-03-03 11:34:41 +0000

    Test Application and Windows Simulator

--- Environment/Simulator/Windows/UserAppSimulator/UserAppSimulator/main.c
+++ Environment/Simulator/Windows/UserAppSimulator/UserAppSimulator/main.c
@@ -5,43 +5,71 @@
 #define LOG_ERROR_ENABLED       1
 #include "SysCall.h"
 
-#ifdef US_AI_GENERATED
-    #include "us_public_headers.inc"
-#else
-    #include "us-Template.h"
-#endif /* US_AI_GENERATED */
+#include "us-AWSIoT.h"
+#include "VendorRPCustomTags.h"
+
+#ifndef RP_CUSTOM_ITEM_AWSIOT_DEVCERT
+#error "RP_CUSTOM_ITEM_AWSIOT_DEVCERT is not defined."
+#endif
+
+#ifndef RP_CUSTOM_ITEM_AWSIOT_PKEY
+#error "RP_CUSTOM_ITEM_AWSIOT_PKEY is not defined."
+#endif
+
+#ifndef AWS_CONNECT_TIMEOUT
+#define AWS_CONNECT_TIMEOUT         50000
+#endif
+
+#ifndef AWS_PUBLISH_TIMEOUT
+#define AWS_PUBLISH_TIMEOUT         10000
+#endif
 
 #define CHECK_US_ERR(_sysStatus, _usStatus) \
                 if (_sysStatus != SysStatus_Success || _usStatus != usStatus_Success) \
                 { LOG_PRINTF(" > us Test Failed. Line %d. Sys Status %d | us Status %d. Exiting the User Application...", __LINE__, _sysStatus, _usStatus); Sys_Exit(); }
 
+#error "Please define the following macros. Embed values in the app or ideally get from the Root Params"
+#define AWS_IOT_URL                 ""
+#define AWS_IOT_THINGNAME           ""
+#define AWS_IOT_TOPIC_TEMP          ""
+
 int main(void)
 {
     SysStatus retVal;
+
+    AWSIoTContext awsIoTCtx;
+    usStatus status;
 
     LOG_PRINTF(" > Container : Microservice Test User App");
 
     SYS_INITIALISE_IPC_MESSAGEBOX(retVal, 4);
 
-#ifdef US_AI_GENERATED
-    #include "us_init_func.inc"
-    #include "us_operation_test.inc"
-#else /* US_AI_GENERATED */
-    us_Template_Initialise();
+    us_AWSIoT_Initialise();
 
+    retVal = us_AWSIoT_Connect(AWS_IOT_URL, AWS_IOT_THINGNAME, RP_CUSTOM_ITEM_AWSIOT_DEVCERT, RP_CUSTOM_ITEM_AWSIOT_PKEY, AWS_CONNECT_TIMEOUT, &awsIoTCtx, &status);
+    CHECK_US_ERR(retVal, status);
+    
+    LOG_PRINTF(" > Connected to AWS IoT Core. Starting to publish temparature periodically...");
+
+    for (int i = 0; ; i++)
     {
-        usStatus status;
-        int32_t a = 5;
-        int32_t b = 6;
-        int32_t expectedResult = a + b;
-        int32_t result = 0;
+        char* payload;
 
-        retVal = us_Template_Sum(a, b, &result, &status);
+        if (i % 2)
+        {
+            payload = "{\"Temp\":27}";
+        }
+        else
+        {
+            payload = "{\"Temp\":28}";
+        }
+
+        retVal = us_AWSIoT_PublishToTopic(&awsIoTCtx, AWS_IOT_TOPIC_TEMP, payload, AWS_PUBLISH_TIMEOUT, &status);
         CHECK_US_ERR(retVal, status);
+        LOG_PRINTF(" > Temparature Published");
 
-        LOG_PRINTF(" > us Sum Test %s", result == expectedResult ? "Success" : "Failed");
+        Sys_Sleep(10000);
     }
-#endif /* US_AI_GENERATED */
 
     LOG_PRINTF(" > Exiting the User Application");
     /* Exit the Container */
